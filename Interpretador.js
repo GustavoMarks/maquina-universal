5ceaf8076542ba791b990a0f306b1b48fd6ac5bc
Gustavo Marques <gustavosmarquesf@gmail.com>
2022-11-29 19:27:24 -0300

    tarefas 2, 3 e 4

--- Interpretador.js
+++ Interpretador.js
@@ -1,6 +1,8 @@
 class Ìnterpretador {
 	constructor() {
-
+		// Tarefa 2 e 3
+		this.PC = 0;
+		this.W = [0];
 	}
 
 	// Tarefa 1: ler instrução
@@ -36,6 +38,45 @@ class Ìnterpretador {
 
 		} catch (error) {
 			throw new Error(`Falha ao ler instrução: ${instrucao}.`);
+		}
+	}
+
+	// Tarefa 2
+	max_var() {
+		return this.W.length;
+	}
+
+	// Tarefa 3
+	// Ex.: 7,2,55,2,31
+	inicializa(entradasString) {
+		try {
+			const entradasArray = String(entradasString).split(",");
+			entradasArray.forEach(entrada => {
+				this.W.push(parseInt(entrada));
+			});
+		} catch (error) {
+			throw new Error(`Falha na incialização com lista de entradas: ${entradasString}`);
+		}
+
+	}
+
+	// Tarefa 4
+	exec_inst(instrucao) {
+		const { tipo, variavel, gotoLinha } = this.ler_instrucao(instrucao);
+		const variavelIndice = parseInt(String(variavel).substring(1)) // removendo "w"
+
+		if (tipo === "incremento") {
+			this.W[variavelIndice] += 1;
+			this.PC += 1;
+
+		} else if (tipo === "decremento") {
+			this.W[variavelIndice] -= 1;
+			this.PC += 1;
+
+		} else {
+			if (this.W[variavelIndice] === 0) this.PC += 1;
+			else this.PC = parseInt(gotoLinha);
+
 		}
 	}
 }
